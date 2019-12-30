34faff26f3be8df4ea22c568efa9392655b76d7b
WWyane <wesleywyane@gmail.com>
2019-12-30 00:23:27 -0800

    user_posts route and pagination

--- scurry/routes.py
+++ scurry/routes.py
@@ -44,7 +44,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route('/home')
 def index():
     form = PostForm()
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', title='Home', posts=posts, form=form)
 
 
@@ -136,6 +137,16 @@ def delete_post(post_id):
     db.session.commit()
     flash('Post has been Deleted', 'success')
     return redirect(url_for('index'))
+
+# Get User Posts
+@app.route('/user/<string:username>')
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
 
 @app.route('/profile', methods=['GET', 'POST'])
 @login_required
